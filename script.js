27a0509d0fb6496e95e801f81b893912ebd36e0f
LHP7-Arnaud7739 <arnaudpesquet@MacBook-Pro-de-Arnaud.local>
2021-11-19 11:49:47 +0100

    Affichage du fichier JSON dans le dossier HTML

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
+let url = 'movies.json'
+let contener = document.getElementById('allMovies')
 
-let url='movies.json'
+fetch(url)
+    .then(Response => Response.json())
+    .then(data => {
 
-fetch (url)
-.then(Response => Response.json())
-.then (data => {
+        data.results.forEach(element => {
+            console.log(element.original_title)
+            console.log(element.id)
+            console.log(element.poster_path)
 
-data.results.forEach(element => {
- console.log(element.original_title) 
- console.log(element.id)
- console.log(element.poster_path)   
-});
+            let film = element
+            let myAffichage = `<div class=" film col-lg-3 ">
+            <div class="row">
+                <div class="affiche col">
+            <img src="${film['poster_path']} "width="100%" height="100%" w
+            alt="">
+            </div>
+            <div class="titreFilm   col-2 ">
+                
+                <h3>${film.original_title}</h3>
+                <div class="synopsis ">
+                    <p>${film.overview}</p>
+                </div>
+                <div class="stars  ">⭐️ ⭐️ ⭐️ ⭐️ ⭐️
+</div>
+                </div>
+                </div>
+            </div>
+        </div>`
 
-})
-let allMovies = document.querySelector("allMovies")
-let requestURL = url
-let request = new XMLHttpRequest()
-request.open ('GET', requestURL)
-request.responseType = 'json'
-request.send()
-request.onload = function (){
+            contener.innerHTML += myAffichage
+        });
 
-    let movies = request.response
-
-}
+    })
